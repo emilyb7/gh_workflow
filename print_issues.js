fc52846b7ccecc60e07d202ca41d68d97a632dbf
emily <limeyb7@gmail.com>
2017-06-19 22:38:48 +0100

    git url comes from git config instead of package.json

--- print_issues.js
+++ print_issues.js
@@ -4,17 +4,18 @@ const fs = require('fs')
 const path = require('path')
 const request = require('request')
 
-const package = fs.readFileSync(path.join(process.cwd(), 'package.json'))
+const gitConfig = fs.readFileSync(path.join(process.cwd(), './.git/config')).toString()
+const repo_url = gitConfig.match(/url\s\=\s[^\n]+/)[0]
+  .split("= ")[1]
 
-const repo = JSON.parse(package).repository.url
-const repo_url = repo.match(/https\:\/\/github\.com\/[\w\-]+\/[\w\-]+/)[0]
-const repo_name = repo_url.match(/[^\/][\w\-]+$/)[0]
-const repo_owner = repo_url.replace("https://github.com/", "").replace(`/${repo_name}`, "")
+const repo_name = repo_url.replace(/\.git$/, '').match(/[^\/][\w\-]+$/)[0]
+const repo_owner = repo_url.replace("git@github.com:", "").replace(/\/.+$/, "")
 
+// get auth token
 const config = fs.readFileSync(path.join(__dirname, 'config.json'))
 const token = JSON.parse(config).token
 
-const query = `{ "query": "query { repository(name\:\\"${repo_name}\\" owner\:\\"${repo_owner}\\") { issues(last:25) { nodes { number, title, url } } }}" }`
+const query = `{ "query": "query { repository(name\:\\"${repo_name}\\" owner\:\\"${repo_owner}\\") { issues(last:25) { totalCount, nodes { number, title, url } } }}" }`
 
 const printEach = ({ number, title, url, }) => `
 ${"\x1b[0m"} #${number} ${"\x1b[35m"} ${title}  ${"\x1b[32m"} ${"\x1b[4m"} ${url}`
@@ -36,9 +37,21 @@ const requestOptions = {
 }
 
 request.post(requestOptions, (err, res, body) => {
+
+  console.log(`Searching for issues for repo ${repo_name} belonging to ${repo_owner}`);
+
+  if (err) return console.log(err);
+
   const parsed = JSON.parse(body)
 
-  if (parsed.errors) return console.log(parsed.error)
+  if (parsed.errors) return console.log(parsed.errors)
 
-  console.log(printIssues(parsed.data.repository.issues.nodes));
+  const totalCount = parsed.data.repository.issues.totalCount;
+
+  const issues = parsed.data.repository.issues.nodes
+
+  console.log(`
+Showing ${issues.length} issue(s) out of total ${totalCount} issue(s) found
+
+    `, printIssues(issues));
 })
