36ed0d578b08b49f1231a20057e302465720658a
emily <limeyb7@gmail.com>
2017-06-19 23:04:12 +0100

    makes script more modular

--- print_issues.js
+++ print_issues.js
@@ -4,18 +4,35 @@ const fs = require('fs')
 const path = require('path')
 const request = require('request')
 
-const gitConfig = fs.readFileSync(path.join(process.cwd(), './.git/config')).toString()
-const repo_url = gitConfig.match(/url\s\=\s[^\n]+/)[0]
-  .split("= ")[1]
-
-const repo_name = repo_url.replace(/\.git$/, '').match(/[^\/][\w\-]+$/)[0]
-const repo_owner = repo_url.replace("git@github.com:", "").replace(/\/.+$/, "")
-
 // get auth token
 const config = fs.readFileSync(path.join(__dirname, 'config.json'))
 const token = JSON.parse(config).token
 
-const query = `{ "query": "query { repository(name\:\\"${repo_name}\\" owner\:\\"${repo_owner}\\") { issues(last:25) { totalCount, nodes { number, title, url } } }}" }`
+const getRepoDetails = dir => {
+
+  if (!fs.exists(path.join(dir, './.git/config'))) {
+    console.log("this directory does not have a git config");
+    return {
+      repo_name: '',
+      repo_owner: '',
+    }
+  }
+
+  const gitConfig = fs
+    .readFileSync(path.join(dir, './.git/config'))
+    .toString()
+
+  const repo_url = gitConfig.match(/url\s\=\s[^\n]+/)[0]
+    .split("= ")[1]
+
+  return {
+    repo_name: repo_url.replace(/\.git$/, '').match(/[^\/][\w\-]+$/)[0],
+    repo_owner: repo_url.replace("git@github.com:", "").replace(/\/.+$/, ""),
+  }
+}
+
+const query = (name, owner) =>
+  `{ "query": "query { repository(name\:\\"${name}\\" owner\:\\"${owner}\\") { issues(last:25) { totalCount, nodes { number, title, url } } }}" }`
 
 const printEach = ({ number, title, url, }) => `
 ${"\x1b[0m"} #${number} ${"\x1b[35m"} ${title}  ${"\x1b[32m"} ${"\x1b[4m"} ${url}`
@@ -27,31 +44,40 @@ const printIssues = (issues, result = '') => {
   return printIssues(issues.slice(1), nextResult)
 }
 
-const requestOptions = {
+const requestOptions = (name, owner) => ({
   uri: "https://api.github.com/graphql",
-  body: query,
+  body: query(name, owner),
   headers: {
     'Authorization' : `bearer ${token}`,
     'User-Agent': 'request',
   },
+})
+
+const getIssues = dir => {
+
+  const { repo_name, repo_owner, } = (getRepoDetails(dir))
+
+  if (!repo_name || !repo_owner) return;
+
+  request.post(requestOptions(repo_name, repo_owner), (err, res, body) => {
+
+    console.log(`Searching for issues for repo ${repo_name} belonging to ${repo_owner}`);
+
+    if (err) return console.log(err);
+
+    const parsed = JSON.parse(body)
+
+    if (parsed.errors) return console.log(parsed.errors)
+
+    const totalCount = parsed.data.repository.issues.totalCount;
+
+    const issues = parsed.data.repository.issues.nodes
+
+    console.log(`
+  Showing ${issues.length} issue(s) out of total ${totalCount} issue(s) found
+
+      `, printIssues(issues));
+  })
 }
 
-request.post(requestOptions, (err, res, body) => {
-
-  console.log(`Searching for issues for repo ${repo_name} belonging to ${repo_owner}`);
-
-  if (err) return console.log(err);
-
-  const parsed = JSON.parse(body)
-
-  if (parsed.errors) return console.log(parsed.errors)
-
-  const totalCount = parsed.data.repository.issues.totalCount;
-
-  const issues = parsed.data.repository.issues.nodes
-
-  console.log(`
-Showing ${issues.length} issue(s) out of total ${totalCount} issue(s) found
-
-    `, printIssues(issues));
-})
+getIssues(process.cwd())
